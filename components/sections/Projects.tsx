3f0f66b6415dd68f6f2c263b3be59e1fed29a678
flowz0 <billylflowers@gmail.com>
2026-03-17 18:08:51 -0700

    Add Projects section component to showcase selected web solutions

---
+++ components/sections/Projects.tsx
@@ -0,0 +1,16 @@
+import SectionContainer from "../layout/SectionContainer";
+
+export default function Projects() {
+  return (
+    <SectionContainer>
+      <div>
+        <h2 className="text-brand950 text-h5 font-bold sm:text-h2">
+          Projects
+        </h2>
+        <p className="text-brand800 text-p mt-4">
+          A selection of projects that showcase my skills in building scalable and impactful web solutions.
+        </p>
+      </div>
+    </SectionContainer>
+  );
+}
