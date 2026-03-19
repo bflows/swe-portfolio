c81887638a2078b9c59f9094ac0b3d225c3a4390
flowz0 <billylflowers@gmail.com>
2026-03-19 12:55:46 -0700

    Refactor Hero component to remove SectionContainer and adjust layout, enhancing the overall structure and spacing

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
-import SectionContainer from "../layout/SectionContainer";
 import { LuChevronDown } from "react-icons/lu";
 
 export default function Hero() {
   return (
-    <SectionContainer>
+    <div id="home" className="mt-64 px-4 scroll-mt-64 md:px-6">
       <h1 className="text-brand950 font-bold text-h4 text-center sm:text-h1">
         Billy Flowers
       </h1>
@@ -21,12 +20,12 @@ export default function Hero() {
         </Link>
       </div>
       {/* Arrow */}
-      <div className="flex flex-col items-center mt-48">
+      <div className="flex flex-col items-center mt-64">
         <p className="text-brand400 uppercase">SCROLL</p>
         <div className="mt-4">
           <LuChevronDown className="text-primary size-8 transition-transform animate-bounce ease-in-out duration-300" />
         </div>
       </div>
-    </SectionContainer>
+    </div>
   );
 }
