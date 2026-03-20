75f1e5ea1a2203a2097122109ebd05ce10127501
flowz0 <billylflowers@gmail.com>
2026-03-19 19:09:25 -0700

    Update SectionContainer component to adjust margin values for improved layout consistency across different screen sizes

--- components/layout/SectionContainer.tsx
+++ components/layout/SectionContainer.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function SectionContainer({ children, id }: { children: ReactNode, id?: string }) {
   return (
-    <div id={id} className="mt-48 max-w-7xl mx-auto px-4 scroll-mt-48">
+    <div id={id} className="mt-32 max-w-7xl mx-auto px-4 scroll-mt-32 md:mt-48 md:scroll-mt-48">
       {children}
     </div>
   );
