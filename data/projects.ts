6ab6ef7cb6a1a22ae01e4605fe727b512c491f03
flowz0 <billylflowers@gmail.com>
2026-03-20 21:55:12 -0700

    Add projects data file to define a list of projects with details including title, description, status, tech stack, and URLs for live site and GitHub repository

---
+++ data/projects.ts
@@ -0,0 +1,40 @@
+import { Project } from "@/types/project";
+
+export const projects: Project[] = [
+  {
+    id: 0,
+    title: "HVAC Blog Content Management System",
+    desc: "Designed and built a full-stack CMS for an HVAC blog. Enabling efficient content creation, editing, and publishing through a custom admin dashboard.",
+    status: "live",
+    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Express', 'MongoDB', '+5 more'],
+    liveUrl: "https://www.ductdaddykc.com/blog",
+    githubUrl: "https://github.com/flowz0/dd-platform"
+  },
+  {
+    id: 1,
+    title: "HVAC Blog Content Management System",
+    desc: "Designed and built a full-stack CMS for an HVAC blog. Enabling efficient content creation, editing, and publishing through a custom admin dashboard.",
+    status: "live",
+    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Express', 'MongoDB', '+5 more'],
+    liveUrl: "https://www.ductdaddykc.com/blog",
+    githubUrl: "https://github.com/flowz0/dd-platform"
+  },
+  {
+    id: 2,
+    title: "HVAC Blog Content Management System",
+    desc: "Designed and built a full-stack CMS for an HVAC blog. Enabling efficient content creation, editing, and publishing through a custom admin dashboard.",
+    status: "live",
+    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Express', 'MongoDB', '+5 more'],
+    liveUrl: "https://www.ductdaddykc.com/blog",
+    githubUrl: "https://github.com/flowz0/dd-platform"
+  },
+  {
+    id: 3,
+    title: "HVAC Blog Content Management System",
+    desc: "Designed and built a full-stack CMS for an HVAC blog. Enabling efficient content creation, editing, and publishing through a custom admin dashboard.",
+    status: "live",
+    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Express', 'MongoDB', '+5 more'],
+    liveUrl: "https://www.ductdaddykc.com/blog",
+    githubUrl: "https://github.com/flowz0/dd-platform"
+  },
+];
