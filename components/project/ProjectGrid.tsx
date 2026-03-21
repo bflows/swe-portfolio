07e191807fd58537994441d26a1d8ebeabcd68a5
flowz0 <billylflowers@gmail.com>
2026-03-21 15:02:01 -0700

    Refactor ProjectGrid to display the last 3 projects in reverse order, improving the visibility of featured projects

--- components/project/ProjectGrid.tsx
+++ components/project/ProjectGrid.tsx
@@ -3,10 +3,13 @@ import ProjectCard from "./ProjectCard";
 
 const MAX_PROJECTS = 3;
 
+/** Last 3 projects in data (reverse order) */
+const featuredProjects = projects.slice(-MAX_PROJECTS).reverse();
+
 export default function ProjectGrid() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-      {projects.slice(0, MAX_PROJECTS).map((project) => (
+      {featuredProjects.map((project) => (
         <ProjectCard key={project.id} {...project} />
       ))}
     </div>
