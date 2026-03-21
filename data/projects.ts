cdfc6713283aa410c8b4867104e458e978e812f6
flowz0 <billylflowers@gmail.com>
2026-03-21 14:58:02 -0700

    Update projects data to include new entries for "Software Engineer Portfolio" and "Field Service Management Platform", enhancing tech stack details and project descriptions

--- data/projects.ts
+++ data/projects.ts
@@ -6,35 +6,26 @@ export const projects: Project[] = [
     title: "HVAC Blog Content Management System",
     desc: "Designed and built a full-stack CMS for an HVAC blog. Enabling efficient content creation, editing, and publishing through a custom admin dashboard.",
     status: "live",
-    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Express', 'MongoDB'],
+    techStack: ['TypeScript', 'Next.js', 'Axios', 'Tailwind CSS', 'Express', 'MongoDB', 'Node.js'],
     liveUrl: "https://www.ductdaddykc.com/blog",
     githubUrl: "https://github.com/flowz0/dd-platform"
   },
   {
     id: 1,
-    title: "HVAC Blog Content Management System",
-    desc: "Designed and built a full-stack CMS for an HVAC blog. Enabling efficient content creation, editing, and publishing through a custom admin dashboard.",
-    status: "live",
-    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Express', 'MongoDB'],
-    liveUrl: "https://www.ductdaddykc.com/blog",
-    githubUrl: "https://github.com/flowz0/dd-platform"
+    title: "Software Engineer Portfolio",
+    desc: "Designed and built a high-performance portfolio using Next.js, featuring a scalable component architecture, accessible UI, and optimized performance for a seamless user experience.",
+    status: "building",
+    techStack: ['TypeScript', 'Next.js', 'Axios', 'Tailwind CSS', 'Express', 'MongoDB', 'Node.js'],
+    // liveUrl: "",
+    githubUrl: "https://github.com/flowz0/swe-portfolio"
   },
   {
     id: 2,
-    title: "HVAC Blog Content Management System",
-    desc: "Designed and built a full-stack CMS for an HVAC blog. Enabling efficient content creation, editing, and publishing through a custom admin dashboard.",
-    status: "live",
-    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Express', 'MongoDB'],
-    liveUrl: "https://www.ductdaddykc.com/blog",
-    githubUrl: "https://github.com/flowz0/dd-platform"
-  },
-  {
-    id: 3,
-    title: "HVAC Blog Content Management System",
-    desc: "Designed and built a full-stack CMS for an HVAC blog. Enabling efficient content creation, editing, and publishing through a custom admin dashboard.",
-    status: "live",
-    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Express', 'MongoDB'],
-    liveUrl: "https://www.ductdaddykc.com/blog",
-    githubUrl: "https://github.com/flowz0/dd-platform"
+    title: "Field Service Management Platform",
+    desc: "Built a full-stack field service platform to streamline dispatching, scheduling, CRM, and payments, replacing third-party tools with a faster, more intuitive system for office staff and technicians.",
+    status: "building",
+    techStack: ['TypeScript', 'Next.js', 'Axios', 'Tailwind CSS', 'Express', 'PostgreSQL', 'Prisma ORM', 'Node.js', 'Stripe', 'Twilio Messaging API'],
+    // liveUrl: "",
+    githubUrl: "https://github.com/flowz0/d-enterprise-suite"
   },
 ];
