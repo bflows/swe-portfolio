e1a19f6f6e193c7e297f18e96527b23179829dcd
flowz0 <billylflowers@gmail.com>
2026-03-20 21:56:02 -0700

    Update ProjectGrid component to dynamically render ProjectCard components based on a projects data array, limiting the display to a maximum of three projects

--- components/project/ProjectGrid.tsx
+++ components/project/ProjectGrid.tsx
@@ -1,13 +1,14 @@
+import { projects } from "@/data/projects";
 import ProjectCard from "./ProjectCard";
+
+const MAX_PROJECTS = 3;
 
 export default function ProjectGrid() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-      <ProjectCard />
-      <ProjectCard />
-      <div className="md:col-span-2 md:w-1/2 md:mx-auto lg:col-span-1 lg:w-fit lg:mx-0">
-        <ProjectCard />
-      </div>
+      {projects.slice(0, MAX_PROJECTS).map((project) => (
+        <ProjectCard key={project.id} {...project} />
+      ))}
     </div>
   );
 }
