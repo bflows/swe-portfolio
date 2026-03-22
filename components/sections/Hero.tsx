e0d0e0fcd0f575bdd46103d7f33b95d8c5f4ad77
flowz0 <billylflowers@gmail.com>
2026-03-21 17:18:53 -0700

    Enhance Hero section styling with gradient text and updated primary color for improved visual appeal

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,10 +4,10 @@ import Button from "../ui/Button";
 export default function Hero() {
   return (
     <div id="home" className="mt-64 px-4 scroll-mt-64 md:px-6">
-      <h1 className="text-brand950 font-bold text-h3 text-center sm:text-h1">
+      <h1 className="font-bold text-h3 text-center bg-linear-to-b bg-clip-text text-transparent from-brand950 to-[#51f0e3] from-40% to-70% sm:text-h1">
         Billy Flowers
       </h1>
-      <h2 className="text-primary font-bold text-h6 text-center mt-4">
+      <h2 className="text-brand950 font-bold text-h6 text-center mt-4">
         Software Engineer
       </h2>
       {/* Buttons */}
