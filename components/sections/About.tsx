328d2b0e8709f35424a6e38d22fb0196a165ae4d
flowz0 <billylflowers@gmail.com>
2026-03-24 01:54:54 -0700

    Update About section to include an ID for improved accessibility and navigation

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -6,7 +6,7 @@ import { about } from "@/data/about";
 
 export default function About() {
   return (
-    <SectionContainer>
+    <SectionContainer id="about">
       <div>
         <h2 className="text-h4 font-bold bg-linear-to-r bg-clip-text text-transparent from-[#51f0e3] to-brand950 from-20% to-25% sm:text-h2 md:from-10% md:to-15%">
           About Me
