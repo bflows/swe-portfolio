20bf8b6075847720a238dcd253f9ca4478b29eb6
flowz0 <billylflowers@gmail.com>
2026-03-24 01:54:06 -0700

    Refactor AboutCard component to accept props for title, description, icon, background color, and text color, enhancing flexibility and customization

--- components/about/AboutCard.tsx
+++ components/about/AboutCard.tsx
@@ -1,16 +1,22 @@
-import { LuBriefcase } from "react-icons/lu";
+import { AboutCardProps } from "@/types/about";
 
-export default function AboutCard() {
+export default function AboutCard({
+  title,
+  desc,
+  Icon,
+  bgColor,
+  textColor
+}: AboutCardProps) {
   return (
-    <div className="flex flex-col items-center justify-center rounded-2xl border border-brand200">
-      <div className="bg-primary/20 p-2 rounded-2xl">
-        <LuBriefcase className="text-primary size-8" />
+    <div className="py-8 w-full flex flex-col items-center justify-center rounded-2xl border border-brand200">
+      <div className={`${bgColor} p-2 rounded-2xl`}>
+        <Icon className={`${textColor} size-8`} />
       </div>
       <h3 className="text-h6 font-bold mt-4 text-brand950">
-        4+ Years
+        {title}
       </h3>
       <p className="text-p mt-2 text-brand800">
-        Software Development
+        {desc}
       </p>
     </div>
   );
