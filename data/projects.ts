600be5994308cdb64fbd20143d45fa9116248407
bflows <billylflowers@gmail.com>
2026-04-27 17:29:37 -0700

    Remove commented-out descriptions for projects in projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -4,10 +4,6 @@ export const projects: Project[] = [
   {
     id: 0,
     title: "HVAC Blog CMS",
-<<<<<<< HEAD
-=======
-    // desc: "Designed and built a full-stack CMS for an HVAC blog. Enabling efficient content creation, editing, and publishing through a custom admin dashboard.",
->>>>>>> 6aa6912bc467a6868fcbb979fa0ae3395867174b
     desc: "Created a content management system to blog for an HVAC company. Custom dashboard to post, update, and view blog posts.",
     status: "live",
     techStack: ['TypeScript', 'Next.js', 'Axios', 'Tailwind CSS', 'Express', 'MongoDB', 'Node.js'],
@@ -17,10 +13,6 @@ export const projects: Project[] = [
   {
     id: 1,
     title: "Software Engineer Portfolio",
-<<<<<<< HEAD
-=======
-    // desc: "Designed and built a high-performance portfolio using Next.js, featuring a scalable component architecture, accessible UI, and optimized performance for a seamless user experience.",
->>>>>>> 6aa6912bc467a6868fcbb979fa0ae3395867174b
     desc: "Designed and developed a responsive portfolio using modern technologies like Next.js to showcase my skills and experience.",
     status: "live",
     techStack: ['TypeScript', 'Next.js', 'Tailwind CSS'],
@@ -30,10 +22,6 @@ export const projects: Project[] = [
   {
     id: 2,
     title: "Field Service Platform",
-<<<<<<< HEAD
-=======
-    // desc: "Built a full-stack field service platform to streamline dispatching, scheduling, CRM, and payments, replacing third-party tools with a faster, more intuitive system for office staff and technicians.",
->>>>>>> 6aa6912bc467a6868fcbb979fa0ae3395867174b
     desc: "Built a field service platform to streamline dispatching, scheduling, CRM, payments, and other business operations efficiently.",
     status: "building",
     techStack: ['TypeScript', 'Next.js', 'Redux', 'Axios', 'Tailwind CSS', 'Express', 'PostgreSQL', 'Prisma ORM', 'Node.js', 'Stripe', 'Twilio Messaging API'],
