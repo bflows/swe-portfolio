9ed6eada4a7de77cc90796632f61ad318c824e63
bflows <billylflowers@gmail.com>
2026-04-28 20:17:08 -0700

    Reorder techStack arrays for consistency across project entries

--- data/projects.ts
+++ data/projects.ts
@@ -6,7 +6,7 @@ export const projects: Project[] = [
     title: "HVAC Blog CMS",
     desc: "Created a content management system to blog for an HVAC company. Custom dashboard to post, update, and view blog posts.",
     status: "live",
-    techStack: ['TypeScript', 'Next.js', 'Axios', 'Tailwind CSS', 'Express', 'MongoDB', 'Node.js'],
+    techStack: ['Node.js', 'Express', 'MongoDB', 'Next.js', 'Axios', 'Tailwind CSS'],
     liveUrl: "https://www.ductdaddykc.com/blog",
     githubUrl: "https://github.com/flowz0/dd-platform"
   },
@@ -15,7 +15,7 @@ export const projects: Project[] = [
     title: "Software Engineer Portfolio",
     desc: "Designed and developed a responsive portfolio using modern technologies like Next.js to showcase my skills and experience.",
     status: "live",
-    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS'],
+    techStack: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Vercel'],
     liveUrl: "https://billyf-portfolio.vercel.app/",
     githubUrl: "https://github.com/flowz0/swe-portfolio"
   },
@@ -24,7 +24,7 @@ export const projects: Project[] = [
     title: "Field Service Platform",
     desc: "Built a field service platform to streamline dispatching, scheduling, CRM, payments, and other business operations efficiently.",
     status: "building",
-    techStack: ['TypeScript', 'Next.js', 'Redux', 'Axios', 'Tailwind CSS', 'Express', 'PostgreSQL', 'Prisma ORM', 'Node.js', 'Stripe', 'Twilio Messaging API'],
+    techStack: ['Express', 'Node.js', 'PostgreSQL', 'Prisma ORM', 'Next.js', 'Redux', 'Axios', 'Tailwind CSS', 'Stripe API', 'Twilio API',],
     // liveUrl: "",
     githubUrl: "https://github.com/flowz0/d-enterprise-suite"
   },
