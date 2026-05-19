be25bf9cc8172754f6465b46366ca27a91227b4d
bflows <billylflowers@gmail.com>
2026-05-19 00:22:07 -0700

    feat: add company logos to experiences for enhanced visual representation

--- data/experiences.ts
+++ data/experiences.ts
@@ -1,5 +1,9 @@
 import { ExperienceCardProps } from "@/types/experience";
 
+import LiftMarkLogo from "@/public/assets/liftmark-logo.png";
+import DuctDaddyLogo from "@/public/assets/Duct-Daddy-logo.png";
+import BVTLogo from "@/public/assets/bay_valley_tech_logo.jpg";
+
 export const experiences: ExperienceCardProps[] = [
     {
       title: "Senior Software Engineer",
@@ -8,21 +12,24 @@ export const experiences: ExperienceCardProps[] = [
       summary:
         "Developed a system supporting scheduling, dispatching, payments, and CRM. Streamlined operations by replacing multiple third-party tools saving $5K annually and improving workflow coordination.",
       tags: ["TypeScript", "Express", "Next.js", "Prisma ORM", "PostgreSQL"],
+      img: LiftMarkLogo
     },
     {
       title: "Full Stack Software Engineer",
       company: "Duct Daddy",
       date: "Nov 2024 - Jul 2025",
       summary:
-        "Built a custom CMS and marketing website enabling scalable content management. Integrated booking systems that helped drive revenue growth 300% in 6 months.",
+      "Built a custom CMS and marketing website enabling scalable content management. Integrated booking systems that helped drive revenue growth 300% in 6 months.",
       tags: ["TypeScript", "Next.js", "MongoDB", "Express", "Node.js"],
+      img: DuctDaddyLogo
     },
     {
       title: "Frontend Software Engineer Intern",
       company: "Bay Valley Tech",
       date: "Mar 2024 - Sep 2024",
       summary:
-        "Created a responsive React application with seamless navigation across devices and adaptive UI components. Integrated RESTful APIs for authentication and data handling in collaboration with backend teams.",
+      "Created a responsive React application with seamless navigation across devices and adaptive UI components. Integrated RESTful APIs for authentication and data handling in collaboration with backend teams.",
       tags: ["React", "JavaScript", "Figma", "UI/UX Design", "RESTful APIs"],
+      img: BVTLogo
     },
   ];
