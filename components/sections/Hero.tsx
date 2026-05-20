ad223dcb7166c8c7c47c9ebdfe1f82a0d965fbe3
bflows <billylflowers@gmail.com>
2026-05-20 01:14:24 -0700

    remove bg gradient in Hero component

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -6,26 +6,25 @@ export default function Hero() {
   return (
     <section id="home" className="relative flex min-h-dvh flex-col scroll-mt-64">
       <div className="hero-background" aria-hidden="true">
-        <div className="absolute inset-0 bg-linear-to-t from-brand100 via-20% via-brand100/0 to-brand100/0" />
       </div>
       <div className="relative z-10 flex flex-1 flex-col px-4 md:px-6">
         <div className="flex flex-1 flex-col items-center justify-center">
-        <h1 className="font-bold text-h3 text-center text-brand950 sm:text-h1">
-          Billy Flowers
-        </h1>
-        <h2 className="text-primary font-bold text-h6 text-center mt-4">
-          Software Engineer
-        </h2>
-        {/* Buttons */}
-        <div className="mt-8 flex items-center justify-center gap-x-2 sm:gap-x-4">
-          <Button href="/#projects" rounded="full">
-            Explore Projects
-            <HiArrowDown className="size-5" />
-          </Button>
-          <Button variant="secondary" href="/files/billy-flowers-resume.pdf" target="_blank" className="lg:hidden" rounded="full">
-            View Resume
-          </Button>
-        </div>
+          <h1 className="font-bold text-h3 text-center text-brand950 sm:text-h1">
+            Billy Flowers
+          </h1>
+          <h2 className="text-primary font-bold text-h6 text-center mt-4">
+            Software Engineer
+          </h2>
+          {/* Buttons */}
+          <div className="mt-8 flex items-center justify-center gap-x-2 sm:gap-x-4">
+            <Button href="/#projects" rounded="full">
+              Explore Projects
+              <HiArrowDown className="size-5" />
+            </Button>
+            <Button variant="secondary" href="/files/billy-flowers-resume.pdf" target="_blank" className="lg:hidden" rounded="full">
+              View Resume
+            </Button>
+          </div>
         </div>
         {/* Arrow */}
         <div className="flex flex-col items-center pb-12">
