9369c38825d314c88180469b5a0c7d94aec1efa4
bflows <billylflowers@gmail.com>
2026-08-22 12:39:07 -0700

    fix: update skills description for clarity and conciseness

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -9,8 +9,8 @@ export default function Skills() {
         <h2 className="text-h4 font-bold text-center bg-linear-to-r bg-clip-text text-transparent from-[#51f0e3] to-brand950 from-35% to-40% sm:text-h2 md:from-40% md:to-45%">
           Skills & Technologies
         </h2>
-        <p className="text-brand800 text-p mt-2 text-center max-w-md mx-auto lg:mt-4">
-          The languages, libraries, frameworks, tools, and methodologies I use to build modern web solutions.
+        <p className="text-brand800 text-p mt-2 text-center max-w-77 mx-auto lg:mt-4">
+          Languages, libraries, frameworks, and tools I use to build modern web solutions.
         </p>
       </div>
       <div className="mt-6 -mx-4 flex items-stretch snap-x snap-mandatory overflow-x-auto scroll-px-4 px-4 pb-2 gap-x-2 gap-y-8 grid-cols-2 sm:px-6 sm:py-5 md:px-8 md:py-7 lg:mt-12 lg:grid-cols-3 sm:rounded-2xl sm:mx-0 sm:grid sm:scroll-px-0 sm:snap-none sm:items-stretch sm:gap-x-4 sm:border sm:bg-brand200/10 sm:border-brand200/20">
