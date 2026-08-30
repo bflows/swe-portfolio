212c7c4786465bb494855c1f4fcea4ed14ce9204
bflows <billylflowers@gmail.com>
2026-08-29 22:05:43 -0700

    fix: update GitHub link in contact section for accuracy

--- data/footer.ts
+++ data/footer.ts
@@ -31,8 +31,8 @@ export const contactLinks: ContactLinkType[] = [
     Icon: BiSolidInbox
   },
   {
-    label: "github.com/flows0",
-    href: "https://github.com/flows0",
+    label: "github.com/bflows",
+    href: "https://github.com/bflows",
     Icon: BiLogoGithub,
     primary: true
   },
