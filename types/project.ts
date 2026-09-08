20fd0f60af38018bc4dbb139c4d35ba934860b35
bflows <billylflowers@gmail.com>
2026-09-08 15:02:47 -0700

    fix: add optional image property to Project type for enhanced project representation

--- types/project.ts
+++ types/project.ts
@@ -1,9 +1,12 @@
+import { StaticImageData } from "next/image";
+
 export type ProjectStatusValue = "building" | "live" | "archived";
 
 export type Project = {
   id: number;
   title: string;
   desc: string;
+  img?: StaticImageData;
   status: ProjectStatusValue;
   techStack: string[];
   liveUrl?: string;
