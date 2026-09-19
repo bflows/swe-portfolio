566aae31182933b935bed519483a1cadbc2e6a6d
bflows <billylflowers@gmail.com>
2026-09-18 21:34:29 -0700

    fix: enhance wording in About section for clarity and impact

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -9,11 +9,10 @@ export default function About() {
         About Me
       </h2>
       <p className="text-h4 font-bold mt-6 text-brand600 sm:text-h2 lg:mt-12">
-        I first started <span className="text-brand950">coding</span> by modding my favorite games. Since then I&apos;ve became a full stack <span className="text-brand950">software engineer</span> capable of building scalable systems end-to-end.
+        I first started <span className="text-brand950">coding</span> by implementing mechanics into my favorite games that I wish they had. That <span className="text-brand950">curiosity</span> led me to complete a programming bootcamp which turned into a <span className="text-brand950">paid</span> internship where I gained professional experience shipping features on a team.
       </p>
       <p className="text-h4 font-bold mt-4 text-brand600 sm:text-h2 lg:mt-6">
-        I&apos;m currently looking for a full stack role <span className="text-brand950">building</span> solutions that make an <span className="text-brand950">impact</span>. Got something you want to <span className="text-brand950">talk</span> about? Connect with me on {" "}
-        <Link href={"https://www.linkedin.com/in/billyflowers/"} target="_blank" className="cursor-pointer group">
+        I&apos;m now a full stack <span className="text-brand950">developer</span> capable of building scalable systems end-to-end. I&apos;m looking for a role to <span className="text-brand950">build</span> solutions that make an impact. Got something you want to talk about? Connect with me on <Link href={"https://www.linkedin.com/in/billyflowers/"} target="_blank" className="cursor-pointer group">
           <FaLinkedin className="inline h-8 w-auto text-brand950 transition-all duration-300 ease-in-out sm:h-12 group-hover:-translate-y-0.5 hover:text-primary" />
         </Link>.
       </p>
